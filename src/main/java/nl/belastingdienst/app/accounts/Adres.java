9f97cdbec4cd68da9506787ce4b685f2572a324f
TJManschot <tj.manschot@belastingdienst.nl>
2021-04-29 10:57:05 +0200

    Adresbuilder + adresvalidatietesten

--- src/main/java/nl/belastingdienst/app/accounts/Adres.java
+++ src/main/java/nl/belastingdienst/app/accounts/Adres.java
@@ -13,6 +13,31 @@ public class Adres implements Valideerbaar {
 
     public boolean valideer() { return true; }
 
+    public Adres() {}
+
+    public Adres(String postcode, String stad, String straat, String huisnummer) {
+        this.postcode = postcode;
+        this.stad = stad;
+        this.straat = straat;
+        this.huisnummer = huisnummer;
+    }
+
+    public static class Builder {
+        private String postcode;
+        private String stad;
+        private String straat;
+        private String huisnummer;
+
+        public Builder postcode(String postcode) { this.postcode = postcode; return this; }
+        public Builder stad(String stad) { this.stad = stad; return this; }
+        public Builder straat(String straat) { this.straat = straat; return this; }
+        public Builder huisnummer(String huisnummer) { this.huisnummer = huisnummer; return this; }
+
+        public Adres build() {
+            return new Adres(postcode, stad, straat, huisnummer);
+        }
+    }
+
     public String getPostcode() {
         return postcode;
     }
